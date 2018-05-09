723d64701f29214e1c887dcf5c9f87ade9671de8
Eric Zhiqi Pan <eric.pan@accedo.tv>
2018-05-09 17:32:54 +0800

    Add test cases for @lazyinit

---
+++ tests/lazyinit.test.js
@@ -0,0 +1,29 @@
+import { lazyinit } from '../src/decorators';
+
+const expensiveOperation = jest.fn(() => {
+  return 100;
+});
+
+class Tester {
+  @lazyinit
+  score = expensiveOperation();
+}
+
+it('does not initialize when instantiation', () => {
+  new Tester();
+  expect(expensiveOperation).toHaveBeenCalledTimes(0);
+});
+
+it('gets initialized at the first time to use it', () => {
+  const tester = new Tester();
+  expect(tester.score).toBe(100);
+  expect(expensiveOperation).toHaveBeenCalledTimes(1);
+});
+
+it('never calculates again after initialization', () => {
+  const tester = new Tester();
+  tester.score;
+  tester.score;
+  tester.score;
+  expect(expensiveOperation).toHaveBeenCalledTimes(1);
+});
