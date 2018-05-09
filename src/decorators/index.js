fa9a76e41292b5edeb797dc77090abf980b6c651
Eric Zhiqi Pan <eric.pan@accedo.tv>
2018-05-09 16:35:24 +0800

    Expose @decorate decorator

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -3,6 +3,7 @@ import log from './log';
 import timer from './timer';
 import lazyinit from './lazyinit';
 import memoize from './memoize';
+import decorate from './decorate';
 import { before, afterReturning, afterThrowing, afterFinally, around } from './aop';
 
 export {
@@ -11,6 +12,7 @@ export {
   timer,
   lazyinit,
   memoize,
+  decorate,
   before,
   afterReturning,
   afterThrowing,
